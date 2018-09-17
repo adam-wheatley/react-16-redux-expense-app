ab071fab2811a6bf00020bdc458a3e1654a03070
adam.wheatley <adam.wheatley@cgi.com>
2018-09-17 17:36:35 +0100

    Edit Expense List Item Format

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => {
     return (
@@ -7,8 +9,8 @@ const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => {
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>Amount: {amount}</p>
-        <p>createdAt: {createdAt}</p>
+        <p>Amount: {numeral(amount).format("$0,00.0")}</p>
+        <p>createdAt: {moment(createdAt).format('MMMM Do, YYYY')}</p>
         </div>
     );
 }
