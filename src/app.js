b83d51513d4b9d199467de66176f8282783037ad
adam.wheatley <adam.wheatley@cgi.com>
2018-09-27 12:43:25 +0100

    Add Async Tests - Setup Test DB

--- src/app.js
+++ src/app.js
@@ -9,11 +9,9 @@ import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
+import './firebase/firebase';
 
 const store = configureStore();
-store.dispatch(addExpense({description: 'rent bill', amount: 4500}));
-store.dispatch(addExpense({description: 'food bill', amount: 20, createdAt: 1000}));
-store.dispatch(addExpense({description: 'water bill', amount: 1500}));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
